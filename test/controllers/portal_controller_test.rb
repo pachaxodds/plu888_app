0ec5538496170c15348f9f4f7933c4e8fe372535
pachax_ <plu@odds.team>
2026-03-08 15:56:20 +0700

    [ADD] setup test

--- test/controllers/portal_controller_test.rb
+++ test/controllers/portal_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class PortalControllerTest < ActionDispatch::IntegrationTest
   test "should get portal" do
-    get portal_portal_url
+    get root_url
     assert_response :success
   end
 end
